907c75e89ce34c304f57f4b348792ce795c00e26
Phong2002 <https://github.com/Phong2002/ROCKET_15.git>
2021-12-20 20:03:14 +0700

    update header and footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Linkedlist from './linkedlist.js'
 
 
 function App() {
+  const linklogo="https://phenikaa-uni.edu.vn:3600/pu/en/modules/logoen.png"
   return (
     <div className="App">
+      <header className="header"><img src={linklogo} /></header>
       <Linkedlist/>
+      <footer className="footer">Copyright © 2021 Lu Minh Phong</footer>
     </div>
   );
 }
